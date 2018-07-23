a8b2f73de1d55b000dc0a1fa9eb170decd216ac3
Teck1 <Teck1@users.noreply.github.com>
2018-07-22 23:03:35 -0700

    Add mashup code for retriving piglatin url.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
@@ -8,21 +7,25 @@ app = Flask(__name__)
 
 
 def get_fact():
-
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
+    return facts[0].getText().strip()
 
-    return facts[0].getText()
+
+def get_piglatin_url(fact):
+    r = requests.post('http://hidden-journey-62459.herokuapp.com/piglatinize/'
+                      , data={'input_text': fact}, allow_redirects=False)
+    return r.headers['Location']
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    url = get_piglatin_url(fact)
+    return '{}<br><a href="{}">{}</a>'.format(fact, url, url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
